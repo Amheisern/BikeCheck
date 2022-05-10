4fad3f8e87cdf61ffa76fea1088120c25426f5a6
lodashreload <icarusjapan@gmail.com>
2022-05-10 15:02:14 -0400

    bicycle model migration

---
+++ Migrations/20220510190139_CreateBicycle.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BikeCheck.Migrations
+{
+    public partial class CreateBicycle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bicycles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Frame = table.Column<string>(type: "text", nullable: true),
+                    Fork = table.Column<string>(type: "text", nullable: true),
+                    Saddle = table.Column<string>(type: "text", nullable: true),
+                    Handlebar = table.Column<string>(type: "text", nullable: true),
+                    BottomBracket = table.Column<string>(type: "text", nullable: true),
+                    ChainRing = table.Column<string>(type: "text", nullable: true),
+                    RearCog = table.Column<string>(type: "text", nullable: true),
+                    Crank = table.Column<string>(type: "text", nullable: true),
+                    WheelSet = table.Column<string>(type: "text", nullable: true),
+                    Pedals = table.Column<string>(type: "text", nullable: true),
+                    Other = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bicycles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bicycles");
+        }
+    }
+}
--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,6 +14,7 @@ namespace BikeCheck.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
+        public DbSet<Bicycle> Bicycles { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
