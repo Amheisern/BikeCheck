308b839d6486be079ab97beeac780377e22f5eed
lodashreload <icarusjapan@gmail.com>
2022-05-10 14:57:38 -0400

    Bicycle model

---
+++ Models/Bicycle.cs
@@ -0,0 +1,24 @@
+namespace BikeCheck.Models
+{
+    public class Bicycle
+    {
+        // need to to add something for images?
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Frame { get; set; }
+        public string Fork { get; set; }
+        public string Saddle { get; set; }
+        public string Handlebar { get; set; }
+        public string BottomBracket { get; set; }
+        public string ChainRing { get; set; }
+        public string RearCog { get; set; }
+        public string Crank { get; set; }
+        public string WheelSet { get; set; }
+        public string Pedals { get; set; }
+        public string Other { get; set; }
+
+
+    }
+}
