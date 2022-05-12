bd205d4a3bcff1d15903ddc233579c13db9459e4
lodashreload <icarusjapan@gmail.com>
2022-05-11 21:27:32 -0400

    added type and fetch function for all bicycles

--- ClientApp/src/pages/Landing.tsx
+++ ClientApp/src/pages/Landing.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { BicycleType } from '../types'
 
 
 export function Landing() {
+const { data: bicycles = []} = useQuery<BicycleType[]>(
+'bicycles',
+async function () {
+  const response = await fetch('/api/Bicycles')
+  //Dont need await since it is react query
+  return response.json()
+}
+)
+console.log({bicycles})
+
   return (
     <>
       <div>
---
+++ ClientApp/src/types.ts
@@ -0,0 +1,24 @@
+//This is code to deal with the star rating 
+import {CSSProperties} from 'react'
+
+export interface CSSStarsProperties extends CSSProperties {
+  '--rating': number
+}
+
+export type BicycleType = {
+  id: number
+  userId: string
+  title: string
+  description: string
+  frame: string
+  fork: string
+  saddle: string
+  handlebar: string
+  bottomBracket: string
+  chainRing: string
+  rearCog: string
+  crank: string
+  wheels: string
+  pedals: string
+  other: string
+}
