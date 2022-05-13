3ab5fb97283adbaf9feeea6ab089033693e80052
lodashreload <icarusjapan@gmail.com>
2022-05-12 21:00:01 -0400

    search function orperational

--- ClientApp/src/pages/Landing.tsx
+++ ClientApp/src/pages/Landing.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import { BicycleType } from '../types'
 import { SingleBicycleFromList } from '../components/SingleBicycleFromList'
 
 export function Landing() {
+  const [filterText, SetFilterText] = useState('')
+
   const { data: bicycles = [] } = useQuery<BicycleType[]>(
-    'bicycles',
+    ['bicycles', filterText],
     async function () {
-      const response = await fetch('/api/Bicycles')
+      let url = '/api/bicycles'
+      if (filterText.length !== 0) {
+        url = `/api/bicycles?filter=${filterText}`
+      }
+      const response = await fetch(url)
       //Dont need await since it is react query
       return response.json()
     }
   )
 
+  console.log(filterText)
+
   return (
     <>
       <div>
+        <form className="search">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={filterText}
+            onChange={function (event) {
+              SetFilterText(event.target.value)
+            }}
+          />
+        </form>
         <section className="container">
           {bicycles.map(function (bicycle) {
             return <SingleBicycleFromList key={bicycle.id} bicycle={bicycle} />
