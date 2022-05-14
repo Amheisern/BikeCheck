9e5b404be675658edcd16c60fb527ddab8acbe2e
lodashreload <icarusjapan@gmail.com>
2022-05-14 15:46:45 -0400

    required certain field

--- Models/Bicycle.cs
+++ Models/Bicycle.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace BikeCheck.Models
 {
     public class Bicycle
@@ -5,9 +8,16 @@ namespace BikeCheck.Models
         // need to to add something for images?
         public int Id { get; set; }
         public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title for your bicycle.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description for your bicycle."), MaxLength(500)]
         public string Description { get; set; }
+
+        [MaxLength(100)]
         public string Frame { get; set; }
+
         public string Fork { get; set; }
         public string Saddle { get; set; }
         public string Handlebar { get; set; }
@@ -17,6 +27,8 @@ namespace BikeCheck.Models
         public string Crank { get; set; }
         public string WheelSet { get; set; }
         public string Pedals { get; set; }
+
+        [MaxLength(500)]
         public string Other { get; set; }
 
 
