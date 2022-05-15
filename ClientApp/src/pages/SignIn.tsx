27b3748552043441cca7ac5ffef8bb8e7dad2266
lodashreload <icarusjapan@gmail.com>
2022-05-15 13:27:03 -0400

    added signIn page

---
+++ ClientApp/src/pages/SignIn.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+
+
+
+
+export function SignIn() {
+  return (
+    <div>
+      <nav>
+        <a href="/">
+          <i className="fa fa-home"></i>
+        </a>
+        <h2>Sign In</h2>
+      </nav>
+      <form action="#">
+        <p className="form-input">
+          <label htmlFor="name">Email</label>
+          <input type="email" name="email" />
+        </p>
+        <p className="form-input">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </p>
+        <p>
+          <input type="submit" value="Submit" />
+        </p>
+      </form>
+    </div>
+  )
+}
