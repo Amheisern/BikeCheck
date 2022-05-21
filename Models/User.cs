19207bce13dc6b1d53ec3f5a1cb34964fcfc66f1
lodashreload <icarusjapan@gmail.com>
2022-05-21 13:08:52 -0400

    updated user model

--- Models/User.cs
+++ Models/User.cs
@@ -15,7 +15,6 @@ namespace BikeCheck.Models
         [JsonIgnore]
         public string HashedPassword { get; set; }
         // Define a property for being able to _set_ a password
-        public List<Bicycle> bicycle { get; set; }
         public string Password
         {
             // Define only the `set` aspect of the property
