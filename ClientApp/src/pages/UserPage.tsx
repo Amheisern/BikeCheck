685ab554addb12955ae84e7422b9c1cfdc22d490
lodashreload <icarusjapan@gmail.com>
2022-05-21 22:34:52 -0400

    added a user

--- ClientApp/src/pages/UserPage.tsx
+++ ClientApp/src/pages/UserPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { getUser } from '../auth'
 
 export function UserPage(){
+  const user = getUser()
 return (
   <div>
-
+    <p className="stable"> {user.fullName} Bicycles </p>
   </div>
 )
 }
