10d612a2dfda2b8b61cd780d1914ed0a79004a8e
lodashreload <icarusjapan@gmail.com>
2022-05-27 16:28:24 -0400

    refactored user bicycle images to get rid of image.

--- ClientApp/src/components/SingleBicycleFromList.tsx
+++ ClientApp/src/components/SingleBicycleFromList.tsx
@@ -3,13 +3,15 @@ import { BicycleType } from '../types'
 // import defaultBikeImage from '../images/default.jpg'
 import defaultUserImage from '../images/logo.png'
 import { Link } from 'react-router-dom'
-
+// onError={(e) => (e.target.src = defaultUserImage)} 
+// need to figure out how to get the default image to display
 // Need to write up this destructuring 1:58:00 in the video
 export function SingleBicycleFromList({ bicycle }: { bicycle: BicycleType }) {
   return (
     <article>
       <Link to={`/bicycles/${bicycle.id}`}>
-        <img src={bicycle.photoURL} width="400" height="400" />
+        <img src={bicycle.photoURL} 
+        width="400" height="400" />
       </Link>
       <ul>
         <li>
--- ClientApp/src/pages/UserPage.tsx
+++ ClientApp/src/pages/UserPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 // import { useQuery } from 'react-query'
 import { Link, useParams } from 'react-router-dom'
 import { getUser } from '../auth'
-import {  BicycleType } from '../types'
+import { BicycleType } from '../types'
 
 // const NullUser: LoggedInUser = {
 //   id: undefined,
@@ -36,48 +36,55 @@ import {  BicycleType } from '../types'
 //   ]
 // }
 
-
-export function UserPage(){
-  const {id} = useParams<{id: string}>()
+export function UserPage() {
+  const { id } = useParams<{ id: string }>()
   const user = getUser()
   const [bicycles, setBicycles] = useState<BicycleType[]>([])
-// const singleUser = LoggedInUser
+  // const singleUser = LoggedInUser
+  useEffect(() => {
+    const loadUserDetails = () => {
+      fetch(`/api/users/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setBicycles(data.bicycles)
+        })
+    }
+    loadUserDetails()
+  }, [id])
+  // async function loadUserDetails() {
+  //   const response = await fetch(`/api/users/${id}`)
+  //   if (response.ok) {
+  //     setBicycles((await response.json()).bicycles)
+  //   } else {
+  //     throw await response.json()
+  //   }
+  // }
+  // useEffect(() => {
+  //   loadUserDetails()
+  // }, [])
+  //   const { data: userDetails = NullUser } = useQuery<BicycleType>(
+  //     ['one-user', id],
+  //     () => loadUserDetails()
+  //   )
+  //   console.log(userDetails);
+  // //  loadUserDetails()
 
-   async function loadUserDetails(){
-     const response = await fetch(`/api/users/${id}`)
-     if (response.ok) {
-      setBicycles ((await response.json()).bicycles)
-     } else {
-       throw await response.json()
-     }
-   }
-useEffect(() => {loadUserDetails()}, [
-])
-//   const { data: userDetails = NullUser } = useQuery<BicycleType>(
-//     ['one-user', id],
-//     () => loadUserDetails()
-//   )
-//   console.log(userDetails);
-// //  loadUserDetails()
-
-return (
-  <div>
-    <h1 className="UserStableName">{user.fullName} stable</h1>
-    <Link to="/add" className="addBicycleLink" >
-      <button className="addBicycle">Add Bicycle</button>
-    </Link>
-    {bicycles.map(bicycle => (
-      <div key={bicycle.id}>
-        <Link to={`/bicycles/${bicycle.id}`}>
-          <h2>{bicycle.title}</h2>
-        </Link>
-        <img src={bicycle.photoURL} alt={bicycle.title} />
-      </div>
-    ))}
-
-    <article>
-</article>
-
-  </div>
-)
+  return (
+    <div>
+      <h1 className="UserStableName">{user.fullName} stable</h1>
+      <Link to="/add" className="addBicycleLink">
+        <button className="addBicycle">Add Bicycle</button>
+      </Link>
+      <article>
+        {bicycles.map((bicycle) => (
+          <div key={bicycle.id}>
+            <Link to={`/bicycles/${bicycle.id}`}>
+              <h2>{bicycle.title}</h2>
+            </Link>
+            <img src={bicycle.photoURL} alt={bicycle.title} />
+          </div>
+        ))}
+      </article>
+    </div>
+  )
 }
